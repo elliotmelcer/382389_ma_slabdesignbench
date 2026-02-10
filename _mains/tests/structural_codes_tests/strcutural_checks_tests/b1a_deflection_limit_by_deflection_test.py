e7e58090c66ac3e9058310145d8e801e668c071c
elliotmmm <3lliotmmm@gmail.com>
2026-02-10 14:18:05 +0100

    added b1a test

---
+++ _mains/tests/structural_codes_tests/strcutural_checks_tests/b1a_deflection_limit_by_deflection_test.py
@@ -0,0 +1,21 @@
+from _mains.testing_files.testing_loads import test_loads
+from _mains.testing_files.testing_slab_construction import test_slab_construction
+from core.analysis_core.checks.structural_checks import DeflectionLimitByDeflectionCheckEC2004DE
+
+"""
+Author: Elliot Melcer
+Testing the calculation of ba1 utilization:
+
+Passed: 16.01.2026
+"""
+
+# --- Check ---
+util = DeflectionLimitByDeflectionCheckEC2004DE.calculateUtilization(
+    test_slab_construction,
+    test_loads,
+    system = "SIMPLE_BEAM",
+    limit_factor=250.,
+    debug = True
+    )
+
+print(f"util = {util:.3f}")
