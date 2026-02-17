6c7476e1311be28ddee55185eec33f31aee325bc
elliotmmm <3lliotmmm@gmail.com>
2026-02-17 16:22:18 +0100

    b1b test PASSED

--- _mains/tests/structural_checks_tests/b1b_deflection_limit_by_mcr_test.py
+++ _mains/tests/structural_checks_tests/b1b_deflection_limit_by_mcr_test.py
@@ -1,14 +1,26 @@
 # --- Check ---
 from _mains.testing_files.testing_loads import test_loads
-from _mains.testing_files.testing_slab_construction import test_slab_construction
+from _mains.testing_files.testing_slab_construction import test_slab_construction_c1_4, test_slab_construction_c1_1
 from core.analysis_core.checks.structural_checks import DeflectionLimitByMcrCheckEC2004DE
 
+"""
+Testing of B.1b Deflection Limit Check by Cracking Moment
+
+Results:
+
+Config | Util Python | Util GH
+-------+-------------+---------
+c1_4   |    0.120    |  0.120 
+c1_1   |    0.692    |  0.703   
+
+passed: 17.02.2026
+"""
+
+print("Testing of B.1b Deflection Limit Check by Cracking Moment")
 util = DeflectionLimitByMcrCheckEC2004DE.calculateUtilization(
-    test_slab_construction,
+    test_slab_construction_c1_4,
     test_loads,
     system = "SIMPLE_BEAM",
     moment = "MAX_POS_MOMENT",
     debug = True
     )
-
-print(f"util = {util:.3f}")
