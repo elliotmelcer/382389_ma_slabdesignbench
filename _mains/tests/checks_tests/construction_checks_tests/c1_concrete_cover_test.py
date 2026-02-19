a70dd816b1cb39d066046ba7b4e526db86046da8
elliotmmm <3lliotmmm@gmail.com>
2026-02-19 17:02:10 +0100

    Moved some things around

--- _mains/tests/checks_tests/construction_checks_tests/c1_concrete_cover_test.py
+++ _mains/tests/checks_tests/construction_checks_tests/c1_concrete_cover_test.py
@@ -13,9 +13,15 @@ passed: 18.02.2026
 from _mains.testing_files.testing_slab_construction import test_slab_construction_c1_1, test_slab_construction_c1_4
 from core.analysis_core.checks.construction_checks import MidlineConcreteCoverCheck
 
-print("Testing of C.1. Sufficient Concrete Cover")
+print("Testing of C.1. Sufficient Concrete Cover \n")
+util = MidlineConcreteCoverCheck.calculateUtilization(
+    test_slab_construction_c1_1
+    )
+
+print(f"c1_util = {util}")
+
 util = MidlineConcreteCoverCheck.calculateUtilization(
     test_slab_construction_c1_4
     )
 
-print(f"c1_util = {util}")
+print(f"c4_util = {util}")
