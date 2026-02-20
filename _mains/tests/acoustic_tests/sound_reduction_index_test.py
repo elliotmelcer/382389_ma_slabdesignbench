8968beed03d58a3f71025fc2bd8a66d828168842
elliotmmm <3lliotmmm@gmail.com>
2026-02-20 19:42:52 +0100

    + standard_impact_sound_pressure_level + test: PASSED
    acoustic_methods.py:
    --------------------
    calculate_sound_reduction_index
     - m_conc = rho * t
     - some comments and refactoring
    
    calculate_standard_impact_sound_pressure_level
     - implemented and tested (PASS)

--- _mains/tests/acoustic_tests/sound_reduction_index_test.py
+++ _mains/tests/acoustic_tests/sound_reduction_index_test.py
@@ -1,20 +1,18 @@
 from _mains.testing_files.testing_slab_construction import test_slab_construction_c1_1, test_slab_construction_c1_3, \
     test_slab_construction_c1_4, test_slab_construction_c1_2_c50
-from core.analysis_core.acoustic_meethods import calculate_sound_reduction_index
+from core.analysis_core.acoustic_methods import calculate_sound_reduction_index
 
 """
 Testing Calculation of Sound Reduction Index R_w
 
 Results for R_w:
 
-Config   | Util Python | Util Python | Util GH
----------+-------------+-------------+----------
-m_conc = |   rho * t   | rho * V / A | rho * t 
----------+-------------+-------------+----------
-c1_1     |    56.29    |    56.32    |   56.29
-c1_2     |    56.75    |    56.81    |   56.75
-c1_3     |    57.24    |    57.28    |   57.24
-c1_4     |    56.78    |    56.85    |   56.78       
+Config   | Util Python |  Util GH
+---------+-------------+-----------
+c1_1     |    56.29    |    56.29
+c1_2     |    56.75    |    56.75
+c1_3     |    57.24    |    57.24
+c1_4     |    56.78    |    56.78       
 
 passed: 20.02.2026
 """
--- core/analysis_core/acoustic_methods.py
+++ core/analysis_core/acoustic_methods.py
@@ -18,36 +18,37 @@ def calculate_sound_reduction_index(
     hp_geometry = hp_shell.hp_geometry
     floor = slab_construction.floor
 
+    "Geometry"
     _B = hp_geometry.B / 1000 # [m]
     _t = hp_geometry.t / 1000 # [m]
     _t_infill = floor.get_layer_by_type(InfillMaterial).thickness / 1000 # [m]
 
-    # Regression Model
+    "Regression Model: Rw"
     regression_Rw = 41.61 + 727.32 * mod_att + 53.55 * _t_infill + 1.51 * _B - 11.52 * _B * _t
 
     insulation_layer = floor.get_layer_by_type(InsulationMaterial)
-    _E_dyn = insulation_layer.material.E_dyn / insulation_layer.thickness * 1000 # MN/m³
+    s = insulation_layer.material.E_dyn / insulation_layer.thickness * 1000 # MN/m³
 
     m_concrete_slab = hp_shell.concrete.density * _t  # kg/m²
-
     m_infill = slab_construction.infill_area_density_kg_m2()                   #kg/m²
     m_screed = floor.get_layer_by_type(ScreedMaterial).area_density_kg_m2()    #kg/m²
 
     m1 = m_infill + m_concrete_slab
     m2 = m_screed
 
-    # Resonant Frequency f0 according to DIN 4109-34 Eq. (1)
-    resonant_frequency_f0 = 160 * (_E_dyn * (1/m1 + 1/m2) ) ** 0.5
+    "Resonant Frequency f0 according to DIN 4109-34 Eq. (1)"
+    resonant_frequency_f0 = 160 * (s * (1/m1 + 1/m2) ) ** 0.5
 
-    # Improvement of the Sound Reduction Index according to nach DIN 4109-34 Table 1
+    "Improvement of the Sound Reduction Index according to nach DIN 4109-34 Table 1"
     delta_R = (74.4 - 20 * math.log10(resonant_frequency_f0)) - (regression_Rw / 2)
 
+    "Sound Reduction Index R_w"
     sound_reduction_index_R_w = regression_Rw + delta_R
 
     if debug:
         print(f"R_w,BG = {regression_Rw:.2f} \n")
 
-        print(f"_E_dyn = {_E_dyn:.2f}")
+        print(f"_E_dyn = {s:.2f}")
         print(f"m_concrete_slab = {m_concrete_slab:.2f}")
         print(f"m_infill = {m_infill:.2f}")
         print(f"m_screed = {m_screed:.2f}")
@@ -56,3 +57,67 @@ def calculate_sound_reduction_index(
         print(f"delta_R = {delta_R:.2f}")
 
     return sound_reduction_index_R_w
+
+def calculate_standard_impact_sound_pressure_level(
+        slab_construction: SlabConstruction,
+        mod_att: float,
+        debug: bool = False
+) -> float:
+    hp_shell = slab_construction.slab.hp_shell
+    hp_geometry = hp_shell.hp_geometry
+    floor = slab_construction.floor
+
+    "Geometry"
+    _B = hp_geometry.B / 1000  # [m]
+    _L = hp_geometry.L / 1000  # [m]
+    _t = hp_geometry.t / 1000  # [m]
+    _Hx = hp_geometry.Hx / 1000  # [m]
+    _Hy = hp_geometry.Hy / 1000  # [m]
+    _t_infill = floor.get_layer_by_type(InfillMaterial).thickness / 1000
+
+    "Material"
+    _Ecm = hp_shell.concrete.Ecm
+
+    "Equivalent Impact Sound Pressure Level L_n_eq_0 of the Concrete Layer"
+    regression_L_n_eq_0 = (259.5925
+                           - 107.5515 * _t
+                           - 6606.6942 * mod_att
+                           - 1.068 * _B
+                           - 41.1633 * _t_infill
+                           - 6.3487 * _Hy
+                           - 0.0043 * _Ecm
+                           + 263.4989 * _t * _t_infill
+                           + 0.1665 * mod_att * _Ecm)
+
+
+    m_screed = floor.get_layer_by_type(ScreedMaterial).area_density_kg_m2()  # kg/m²
+    m2 = m_screed
+
+    insulation_layer = floor.get_layer_by_type(InsulationMaterial)
+    s = insulation_layer.material.E_dyn / insulation_layer.thickness * 1000 # MN/m³
+
+    "??? according to DIN 4109-34 Eq.(3)"
+    delta_L_w = 13 * math.log10(m2) - 14.2 * math.log10(s) + 20.8
+
+    "Correctional Value K for Flanking Sound Transmission"
+    K = 0.0    # not considered
+
+    "Weighted Standard Impact Sound Pressure Level"
+    L_nw = regression_L_n_eq_0 - delta_L_w + K
+
+    if debug:
+        print(f"B = {_B}")
+        print(f"L = {_L}")
+        print(f"t = {_t}")
+        print(f"Hx = {_Hx}")
+        print(f"Hy = {_Hy}")
+        print(f"t_infill = {_t_infill}")
+        print(f"Ecm = {_Ecm}")
+
+        print(f"L_n_eq = {regression_L_n_eq_0:.2f}")
+
+        print(f"delta_L_w = {delta_L_w:.2f}")
+
+        print(f"L_nw = {L_nw:.2f}")
+
+    return L_nw
