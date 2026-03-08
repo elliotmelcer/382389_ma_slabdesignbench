78ce2306683a75f235916a3c11a8fcaaf2347892
elliotmmm <3lliotmmm@gmail.com>
2026-03-08 17:37:27 +0100

    + WIP: B1a and B2a high discrepancies between python and grasshopper +

--- _mains/tests/checks_tests/structural_checks_tests/b1a_deflection_limit_by_deflection_test.py
+++ _mains/tests/checks_tests/structural_checks_tests/b1a_deflection_limit_by_deflection_test.py
@@ -1,5 +1,6 @@
 from _mains.testing_files.testing_loads import test_loads
-from _mains.testing_files.testing_slab_construction import test_slab_construction_c1_4
+from _mains.testing_files.testing_slab_construction import test_slab_construction_c1_4, test_slab_construction_c1_1, \
+    test_slab_construction_c1_2_c50, test_slab_construction_c1_2_c80, test_slab_construction_c1_3
 from core.analysis_core.checks.structural_checks import DeflectionLimitByDeflectionCheckEC2004DE
 
 """
@@ -7,6 +8,27 @@ Author: Elliot Melcer
 Testing the calculation of ba1 utilization:
 
 Passed: 10.02.2026
+
+Methodology:
+ 1. Run deflection_verification.py
+ 2. Use Printout as input for the Grasshopper Function "M-Kappa V2" 
+ 3. Plug Deflection into B.1a Check in Grasshopper -> Util GH
+ 4. Run this file: -> Util Python
+ 5. Compare Utilization
+
+
+Results:
+
+Config   | Util GH  | Util Python* |  Diff
+---------+----------+--------------+-------
+c1_1     |   0.317  |     0.299    |   -6.0 % 
+c1_2_c50 |  -0.308  |    -0.310    |   +0.5 % 
+c1_2_c80 |  -0.252  |    -0.255    |   +1.2 %   
+c1_3     |   0.170  |     0.505    | +197.0 %   
+c1_4     |  -0.311  |    -0.311    |    0.0 %
+
+*raw utilization
+
 """
 
 # --- Check ---
@@ -18,4 +40,4 @@ util = DeflectionLimitByDeflectionCheckEC2004DE.calculateUtilization(
     debug = True
     )
 
-print(f"util = {util:.3f}")
+print(f"screened util = {util:.3f}")
--- _mains/tests/checks_tests/structural_checks_tests/b2a_failure_announcement_by_deflection.py
+++ _mains/tests/checks_tests/structural_checks_tests/b2a_failure_announcement_by_deflection.py
@@ -1,26 +1,38 @@
 from _mains.testing_files.testing_loads import test_loads
-from _mains.testing_files.testing_slab_construction import test_slab_construction_c1_4, test_slab_construction_c1_1
+from _mains.testing_files.testing_slab_construction import test_slab_construction_c1_4, test_slab_construction_c1_1, \
+    test_slab_construction_c1_2_c50, test_slab_construction_c1_2_c80, test_slab_construction_c1_3
 from core.analysis_core.checks.structural_checks import FailureAnnouncementByDeflectionCheckEC2004DE
 
 """
 Testing of B.2a Failure Announcement by Deflection
 
+Methodology:
+ 1. Run deflection_verification.py
+ 2. Use Printout as input for the Grasshopper Function "M-Kappa V2" 
+ 3. Plug Deflection into B.2a Check in Grasshopper -> Util GH
+ 4. Run this file: -> Util Python
+ 5. Compare Utilization
+
+Note: Utilization is given before as raw w_min/w
+
 Results:
 
-Config | Util Python | Util GH
--------+-------------+---------
-c1_4   |   99.000    |  
-c1_1   |    1.109    |     
+Config   | Util Python | Util GH  |  Diff
+---------+-------------+----------+-------
+c1_1     |     1.11    |    0.92  | -17 %
+c1_2_c50 |   -21.48    |  -22.59  |   5 %
+c1_2_c80 |   -25.62    |  -28.34  |  11 %
+c1_3     |     2.68    |    3.83  |  43 %
+c1_4     |   -10.35    |  -10.49  |   1 %
+  
 
-passed: 
+passed: 03.03.26
 """
 
 util = FailureAnnouncementByDeflectionCheckEC2004DE.calculateUtilization(
-    test_slab_construction_c1_4,
+    test_slab_construction_c1_1,
     test_loads,
     system = "SIMPLE_BEAM",
     min_factor = 100.,
     debug = True
     )
-
-print(f"util = {util:.3f}")
