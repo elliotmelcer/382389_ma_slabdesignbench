7b2111efb3c92979cdaded92aee924cc40130610
elliotmmm <3lliotmmm@gmail.com>
2026-03-08 16:48:20 +0100

    + PASSED: Testing of A Ultimate Moment +

--- _mains/tests/checks_tests/structural_checks_tests/a_ultimate_moment_check_test.py
+++ _mains/tests/checks_tests/structural_checks_tests/a_ultimate_moment_check_test.py
@@ -1,63 +1,47 @@
-from matplotlib import pyplot as plt
-
-from _mains.testing_files.testing_loads import test_loads
-from _mains.testing_files.testing_slab_construction import test_slab_construction_c1_4, test_slab_construction_c1_1, \
-    test_slab_construction_c1_2_c50, test_slab_construction_c1_2_c80, test_slab_construction_c1_3, \
-    test_slab_construction_ref
 from core.analysis_core.checks.structural_checks import UltimateMomentCheckEC2004DE
-
-util_c1_1 = UltimateMomentCheckEC2004DE.calculateUtilization(
+from _mains.testing_files.testing_loads import test_loads
+from _mains.testing_files.testing_slab_construction import (
     test_slab_construction_c1_1,
-    test_loads,
-    "SIMPLE_BEAM",
-    "MAX_POS_MOMENT",
-    debug_print=False
-)
-
-util_c1_2_c50 = UltimateMomentCheckEC2004DE.calculateUtilization(
     test_slab_construction_c1_2_c50,
-    test_loads,
-    "SIMPLE_BEAM",
-    "MAX_POS_MOMENT",
-    debug_print=False
-)
-
-util_c1_2_c80 = UltimateMomentCheckEC2004DE.calculateUtilization(
     test_slab_construction_c1_2_c80,
-    test_loads,
-    "SIMPLE_BEAM",
-    "MAX_POS_MOMENT",
-    debug_print=False
-)
-
-util_c1_3 = UltimateMomentCheckEC2004DE.calculateUtilization(
     test_slab_construction_c1_3,
-    test_loads,
-    "SIMPLE_BEAM",
-    "MAX_POS_MOMENT",
-    debug_print=False
-)
-
-util_c1_4 = UltimateMomentCheckEC2004DE.calculateUtilization(
     test_slab_construction_c1_4,
-    test_loads,
-    "SIMPLE_BEAM",
-    "MAX_POS_MOMENT",
-    debug_print=False
+    test_slab_construction_ref,
 )
 
-util_ref = UltimateMomentCheckEC2004DE.calculateUtilization(
-    test_slab_construction_ref,
-    test_loads,
-    "SIMPLE_BEAM",
-    "MAX_POS_MOMENT",
-    debug_print=False
-)
+"""
+Testing of A Ultimate Moment
+
+Results:
+
+Config   | Util Python | Util GH  |  Diff
+---------+-------------+----------+---------
+c1_1     |    1.069    |   1.048  |  -1.96 %
+c1_2_c50 |    0.348    |   0.345  |  -0.86 %
+c1_2_c80 |    0.307    |   0.295  |  -3.91 %
+c1_3     |    0.421    |   0.408  |  -3.09 %
+c1_4     |    0.181    |   0.178  |  -1.66 %
+ 
+
+passed: 08.03.26
+"""
+
+test_cases = {
+    "C1_1": test_slab_construction_c1_1,
+    "C1_2_c50": test_slab_construction_c1_2_c50,
+    "C1_2_c80": test_slab_construction_c1_2_c80,
+    "C1_3": test_slab_construction_c1_3,
+    "C1_4": test_slab_construction_c1_4,
+}
 
 print("Testing Check A: Ultimate Moment")
-print(f"util C1_1:      {util_c1_1:.3f}")
-print(f"util C1_2_c50:  {util_c1_2_c50:.3f}")
-print(f"util C1_2_c80:  {util_c1_2_c80:.3f}")
-print(f"util C1_3:      {util_c1_3:.3f}")
-print(f"util C1_4:      {util_c1_4:.3f}")
-print(f"util Ref :      {util_ref:.3f}")
+
+for name, slab_construction in test_cases.items():
+    util = UltimateMomentCheckEC2004DE.calculateUtilization(
+        slab_construction,
+        test_loads,
+        "SIMPLE_BEAM",
+        "MAX_POS_MOMENT",
+        debug_print=False,
+    )
+    print(f"util {name:<8}: {util:.3f}")
