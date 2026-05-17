65d1a3812738b4789f8fc0230bd5bf96cb0c6c0e
elliotmmm <3lliotmmm@gmail.com>
2026-05-17 23:29:54 +0200

    refactors

--- core/ioh_core/experiment.py
+++ core/ioh_core/experiment.py
@@ -8,44 +8,44 @@ def run_experiment(problem_bundle: dict[str, dict], algorithm, n_runs: int):
     out_root = Path(os.getcwd()) / "logger_results"
     out_root.mkdir(parents=True, exist_ok=True)
 
-    for pid, b in problem_bundle.items():
-        p = b["problem"]
-        ctx = b["ctx"]
-        var_names = b["var_names"]
-        constraint_names = b["active_constraint_names"]
+    for problem_id, bundle in problem_bundle.items():
+        problem = bundle["problem"]
+        eval_context = bundle["ctx"]
+        var_names = bundle["var_names"]
+        constraint_names = bundle["active_constraint_names"]
 
-        ctx.ensure_constraints(constraint_names)
-        ctx.ensure_params(var_names)
+        eval_context.ensure_constraints(constraint_names)
+        eval_context.ensure_params(var_names)
         print("Logging root:", out_root.resolve(),"\n")
 
         trigger_alw = [ltr.Each(1)]
-        lg = ioh.logger.Analyzer(
+        logger = ioh.logger.Analyzer(
             root=str(out_root),
             folder_name="my-experiment",
             algorithm_name=algorithm.name,
             store_positions=False,
             triggers=trigger_alw,
         )
-        lg.watch(ctx, "y")
-        lg.watch(ctx, "y_p")
-        lg.watch(ctx, "misses")
-        lg.watch(ctx, "hits")
+        logger.watch(eval_context, "y")
+        logger.watch(eval_context, "y_p")
+        logger.watch(eval_context, "misses")
+        logger.watch(eval_context, "hits")
 
-        for c in constraint_names:
-            lg.watch(ctx, f"c__{c}")
+        for constr in constraint_names:
+            logger.watch(eval_context, f"c__{constr}")
 
-        for v in var_names:
-            lg.watch(ctx, f"var__{v}")
+        for var in var_names:
+            logger.watch(eval_context, f"var__{var}")
 
-        p.attach_logger(lg)
+        problem.attach_logger(logger)
 
         for run in range(n_runs):
-            algorithm(p)
-            print(f"[{p.meta_data.name} run {run + 1} - best found:{p.state.current_best.y: .3f}")
-            best_x_overall = list(p.state.current_best.x)
-            ctx.reset()
-            p.reset()
-            _ = p(best_x_overall)
+            algorithm(problem)
+            print(f"[{problem.meta_data.name} run {run + 1} - best found:{problem.state.current_best.y: .3f}")
+            best_x_overall = list(problem.state.current_best.x)
+            eval_context.reset()
+            problem.reset()
+            _ = problem(best_x_overall)
 
-        p.detach_logger()
-        lg.close()
+        problem.detach_logger()
+        logger.close()
