dcb16854c4fdce0c37db04171a26357019b3bea5
elliotmmm <3lliotmmm@gmail.com>
2026-05-25 22:02:41 +0200

    some more testing

--- _mains/tests/checks_tests/construction_checks_tests/c1_concrete_cover_test.py
+++ _mains/tests/checks_tests/construction_checks_tests/c1_concrete_cover_test.py
@@ -10,18 +10,25 @@ c1_4   |   0.2467    |  0.2467
 
 passed: 18.02.2026
 """
-from _mains.testing_files.testing_slab_construction import test_slab_construction_c1_1, test_slab_construction_c1_4
+from _mains.testing_files.testing_slab_construction import test_slab_construction_c1_1, test_slab_construction_c1_4, \
+    test_slab_construction_ref
 from core.analysis_core.checks.construction_checks import MidlineConcreteCoverCheck
 
-print("Testing of C.1. Sufficient Concrete Cover \n")
+# print("Testing of C.1. Sufficient Concrete Cover \n")
+# util = MidlineConcreteCoverCheck.calculate_utilization(
+#     test_slab_construction_c1_1
+#     )
+#
+# print(f"c1_util = {util}")
+#
+# util = MidlineConcreteCoverCheck.calculate_utilization(
+#     test_slab_construction_c1_4
+#     )
+
+# print(f"c4_util = {util}")
+
 util = MidlineConcreteCoverCheck.calculate_utilization(
-    test_slab_construction_c1_1
+    test_slab_construction_ref
     )
 
-print(f"c1_util = {util}")
-
-util = MidlineConcreteCoverCheck.calculate_utilization(
-    test_slab_construction_c1_4
-    )
-
-print(f"c4_util = {util}")
+print(f"ref_util = {util}")
