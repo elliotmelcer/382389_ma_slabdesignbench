2297fedfd841aa854db39dc62101da3bf20092ce
elliotmmm <3lliotmmm@gmail.com>
2026-06-02 21:38:18 +0200

    BUG FIX two span moment calculation

--- _mains/tests/structuralcodes_tests/moment_at_x_and_max_test.py
+++ _mains/tests/structuralcodes_tests/moment_at_x_and_max_test.py
@@ -1,8 +1,12 @@
 # For SIMPLE_BEAM - both methods work:
+import numpy as np
+from matplotlib import pyplot as plt
+
 from _mains.testing_files.testing_loads import test_loads
 from _mains.testing_files.testing_slab_construction import test_slab_construction_c1_4
 from core.analysis_core.statics.constants import SystemType, MomentType
 from core.analysis_core.statics.internal_forces import InternalForces
+from core.visualization_core.visualization import PlotLine, plot_moment_curvature_multiple
 
 x=0.25
 
@@ -26,9 +30,28 @@ M_max_three_span = InternalForces.calculate_moment_kNm(
     moment=MomentType.MAX_POS_MOMENT  # ✓ Works!
 )
 
+# For TWO_SPAN:
+xs = np.linspace(0, 2, 101)
+m_two_span_list = []
+for x in xs:
+    M_max_mid_support = InternalForces.calculate_moment_kNm(
+        test_slab_construction_c1_4,
+        test_loads,
+        system=SystemType.TWO_SPAN,
+        x_norm=x
+    )
+    m_two_span_list.append(M_max_mid_support)
+
+M_Line = [PlotLine(m_two_span_list, xs.tolist(), color='red', linestyle='dashed', name = "M")]
+
+plot_moment_curvature_multiple(M_Line)
+
+plt.show()
+
 print(f"M(x={x}) = {M_at_quarter:.2f}")
 print(f"M_max = {M_max:.2f}")
 print(f"M_max_three_span = {M_max_three_span:.2f}")
+print(f"M_max_mid_support = {M_max_mid_support:.2f}")
 
 # # This would raise NotImplementedError:
 # M_at_pos = InternalForces.calculate_moment(
